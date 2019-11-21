b15d2c57a7d1ae6d50a0fdbc7ae864aa892adf55
Abel Dimas <avtw37@gmail.com>
2019-11-21 09:57:30 -0800

    added word list

---
+++ main.js
@@ -0,0 +1,48 @@
+// Globals
+let time = 5;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+const words = [
+  'joke',
+  'space',
+  'laughter',
+  'siblings',
+  'master',
+  'definition',
+  'grettings',
+  'nutrition',
+  'investigate',
+  'horrendous',
+  'symptom',
+  'laughter',
+  'developer',
+  'engineer',
+  'hero',
+  'runaway',
+  'cocktail',
+  'serious',
+  'monarch',
+  'biology',
+  'geometry',
+  'algebra',
+  'science',
+  'history',
+  'programmer',
+  'architect',
+  'horizon',
+  'peace'
+];
+
+// Initialize Game
+function init() {
+  
+}
